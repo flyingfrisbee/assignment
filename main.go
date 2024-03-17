63297f673696cdd5c5dd159138760caa392d0ab5
Giovann <giovann@bangunindo.com>
2024-03-17 16:45:28 +0700

    setup env

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Set time to UTC+7
+	jktLocation, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Local = jktLocation
+
+	if len(os.Args) < 1 {
+		// Load env
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
